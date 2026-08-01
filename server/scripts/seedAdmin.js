989a31bd8c1cc7684adda19e449303ace1c76b01
Giridhar Reddy Parlapalli <95241071+Giridhar113@users.noreply.github.com>
2026-08-01 15:22:02 +0530

    Load local env for admin seed

--- server/scripts/seedAdmin.js
+++ server/scripts/seedAdmin.js
@@ -1,6 +1,8 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../.env.local") });
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env.local") });
 require("dotenv").config();
 
 const bcrypt = require("bcryptjs");
